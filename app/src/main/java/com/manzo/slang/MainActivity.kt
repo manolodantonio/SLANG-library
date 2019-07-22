edb3f22c1cfda9e97bb58501c7c8cdd8e5b1f493
m.dantonio <manolodantonio@gmail.com>
2019-07-22 14:39:17 +0200

    initial commit

--- app/src/main/java/com/manzo/slang/MainActivity.kt
+++ app/src/main/java/com/manzo/slang/MainActivity.kt
@@ -2,6 +2,7 @@ package com.manzo.slang
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.manzo.slang.navigation.init
 
 class MainActivity : AppCompatActivity() {
 
