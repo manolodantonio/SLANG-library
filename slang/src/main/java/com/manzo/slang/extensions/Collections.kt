845fae5c9866d83fd48834b665b7839a8726442f
m.dantonio <manolodantonio@gmail.com>
2019-09-12 18:04:37 +0200

    add collections remove replace

--- slang/src/main/java/com/manzo/slang/extensions/Collections.kt
+++ slang/src/main/java/com/manzo/slang/extensions/Collections.kt
@@ -31,7 +31,46 @@ fun <OB> List<OB>.addIfUnique(item: OB): Boolean {
     return item.addIfUnique(this as MutableList<OB>)
 }
 
-
+/**
+ * Cast list to mutable list, or returns new empty list if original list is empty (list cannot be cast to mutable if empty)
+ * @receiver List<T>
+ * @return MutableList<T>
+ */
 fun <T> List<T>.asSafeMutable(): MutableList<T> {
+    //todo see if empty list can be cast to mutable specifying the type -> this as MutableList<T>
     return (if (isNotEmpty()) this as MutableList else mutableListOf())
 }
+
+/**
+ * Replace with the provided newValue every object in the list that matches the conditionBlock
+ * @receiver MutableList<T>
+ * @param newValue T
+ * @param conditionBlock Function1<[@kotlin.ParameterName] T, Boolean>
+ */
+fun <T> MutableList<T>.replace(newValue: T, conditionBlock: (listElement: T) -> Boolean) {
+    forEachIndexed { index, element ->
+        if (conditionBlock(element)) add(index, newValue)
+    }
+}
+
+/**
+ * Removes the first element that matches the condition block.
+ *
+ * @receiver MutableList<T>
+ * @param conditionBlock Function1<T, Boolean>
+ * @return True if condition is found and removal is successful.
+ */
+fun <T> MutableList<T>.removeFirst(conditionBlock: (listElement: T) -> Boolean): Boolean {
+    return find { conditionBlock.invoke(it) }?.let { remove(it) } ?: false
+}
+
+/**
+ * Remove all elements in the list that match the conditionBlock
+ *
+ * @receiver MutableList<T>
+ * @param conditionBlock Function1<T, Boolean>
+ */
+fun <T> MutableList<T>.remove(conditionBlock: (listElement: T) -> Boolean) {
+    forEach { if (conditionBlock(it)) remove(it) }
+}
+
